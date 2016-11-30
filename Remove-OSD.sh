22dd37efa67543ff97698c7cd4e6d89b82080fcb
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2016-11-30 11:51:33 -0500

    Add files via upload

--- Remove-OSD.sh
+++ Remove-OSD.sh
@@ -56,7 +56,7 @@ echo "-----------------";
 echo "--- IMPORTANT ---";
 echo "-----------------";
 echo
-echo "--- Once the Ceph cluster health is HEALTH_OK (ceph -s), please run:";
+echo "--- Once the Ceph cluster as no more (recovery count or objects degraded percentage (ceph -s)), please run:";
 echo "------ ceph auth del osd.$osd_id";
 echo "------ ceph osd rm osd.$osd_id";
 echo
